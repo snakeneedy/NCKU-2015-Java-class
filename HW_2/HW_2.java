f97afd4c8dee10e5d7d860273cc48c6ab8c55117
snakeneedy <snakeneedy@gmail.com>
2015-03-25 14:41:56 +0800

    C: HW_2 字串分析與取代

---
+++ HW_2/HW_2.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class HW_2 {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner( System.in );
+		System.out.printf("Input a filename: ");
+		String filename = scanner.nextLine();
+
+		int lineCount = countFileLines( filename );
+		String resString = stringStream( filename );
+
+		// replace "hate" with "love"
+		if( lineCount % 2 == 1 ) {
+			// odd lines
+			resString = resString.replaceFirst( "hate", "love" );
+		}
+		else {
+			// even lines
+			int i = resString.lastIndexOf("hate");
+			resString = (resString.substring(0, i) + "love" + resString.substring( i+4 ));
+		}
+
+		// replace all "Java" with "Java Software Development"
+		String copyString = resString, tmpString = resString, oriString = resString;
+		resString = "";
+		tmpString = tmpString.toUpperCase();
+		int index = (-1), preIndex = (-1);
+		while( (index = tmpString.indexOf("JAVA")) != (-1) ) {
+			preIndex = index;
+			resString = resString + copyString.substring(0, index) + "Java Software Development";
+			// TODO
+			copyString = copyString.substring(index + 4);
+			tmpString = tmpString.substring(index + 4);
+		}
+		if( preIndex==index ) {
+			// no replacement is processed
+			resString = oriString;
+		}
+		else {
+			resString = resString + copyString;
+		}
+
+		// result
+		System.out.print( resString );
+	}// end of main()
+
+	static int countFileLines( String filename ) {
+		Scanner scanner = null;
+		int num = 0;
+		try{
+			scanner = new Scanner( new FileInputStream( filename ) );
+			while( scanner.hasNextLine() ){
+				scanner.nextLine();
+				num++;
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("FileNotFoundException");
+		}
+		return num;
+	}
+
+	static String stringStream ( String filename ) {
+		String str = "";
+		try {
+			Scanner fileScanner = new Scanner( new FileInputStream( filename ) );
+			while( fileScanner.hasNextLine() ){
+				str = str + fileScanner.nextLine() + "\n";
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("FileNotFoundException");
+		}
+		return str;
+	}
+}
+// end of java
--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 NCKU-2015-Java-class
 ==========
 
-## HW_1
+HW_1
+-----
 給定兩個字串，先將個別字串首字元移到尾端，並加上"ay"，形成新字串；再將個別新字串首字大寫，才將兩字串以空格隔開成一個新字串。
 > System.out.print 的使用，String 的串接，和如何使用 substring(), toUpperCase()
+
+HW_2
+-----
+1. 給定一個文字檔，若內容行數為奇數，取代第一個"hate"為"love"；若為偶數，取代最後一行的"hate"為"love"。
+2. 取代"JAVA"為"Java Software Development"，任意大小寫都要考慮，意即"jAva"和"JavA"都要取代。
+> HW_1 的技能加上 String 的函數使用。
+> indexOf( String ), lastIndexOf( String )
+> **要 catch FileNotFoundException**
 
 - - -
