386a2410953a2f4831ad3319be84b8f3286c93db
snakeneedy <snakeneedy@gmail.com>
2015-04-15 18:35:18 +0800

    U: HW_4 功能完成

--- HW_4/HW_4.java
+++ HW_4/HW_4.java
@@ -1,11 +1,85 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class HW_4 {
 	public static void main (String args[]) {
-		;
+		ArrayList<String> filenameList = new ArrayList<String>();
+		filenameList.add( "hw4_file1.csv" );
+		filenameList.add( "hw4_file2.csv" );
+		//System.out.println("[debug] " + filenameList );
+		try {
+			Product[] productsArray = null;
+			for ( int ti=0; ti<filenameList.size(); ++ti ) {
+				StringBuilder id = new StringBuilder(
+						Integer.toString( ti + 1 )
+					);
+				while ( id.length() < 3 ) {
+					id.insert( 0, "0" );
+				}
+				System.out.println("---" + id.toString() + "---");
+				productsArray = analyzeFile( filenameList.get(ti) );
+				Arrays.sort( productsArray );
+				for (int i=0; i<productsArray.length; ++i) {
+					productsArray[i].printFormatted();
+				}
+			}
+			//for ( int ti=0; ti<args.length; ++ti ) {
+			//	StringBuilder id = new StringBuilder(
+			//			Integer.toString( ti + 1 )
+			//		);
+			//	while ( id.length() < 3 ) {
+			//		id.insert( 0, "0" );
+			//	}
+			//	System.out.println("---" + id.toString() + "---");
+			//	productsArray = analyzeFile( args[ti] );
+			//	Arrays.sort( productsArray );
+			//	for (int i=0; i<productsArray.length; ++i) {
+			//		productsArray[i].printFormatted();
+			//	}
+			//}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}// main
 	static Product[] analyzeFile (String filename)
 			throws Exception {
-		Product[] productArray = null;
-		// TODO
-		return productArray;
+		// init
+		Scanner inputScanner = new Scanner( new FileInputStream( filename ) );
+		String inputLine = null;
+		String[] nameArray = null;
+		String[] rateStringArray = null;
+//System.out.println("[debug] content in '" + filename + "'");
+//while( inputScanner.hasNextLine() ) {
+//	System.out.println( inputScanner.nextLine() );
+//}
+		// name
+		if ( inputScanner.hasNextLine() ) {
+			inputLine = inputScanner.nextLine();
+			nameArray = inputLine.split(",");
+		}
+		//System.out.println("[debug] nameArray.length = " + nameArray.length);
+		Product[] productsArray = new Product[ nameArray.length ];
+		//System.out.println("[debug] productsArray.length = " + productsArray.length);
+		for ( int i=0; i<nameArray.length; ++i ) {
+			productsArray[i] = new Product();
+			//productsArray[i].printFormatted();//TODO debug
+			productsArray[i].setName( nameArray[i] );
+		}
+		// rate
+		while ( inputScanner.hasNextLine() ) {
+			inputLine = inputScanner.nextLine();
+			rateStringArray = inputLine.split(",");
+			for ( int i=0; i<nameArray.length; ++i ) {
+				if ( rateStringArray[i].length() > 0 ) {
+					productsArray[i].addRate(
+							Integer.parseInt( rateStringArray[i] )
+						);
+				}
+			}
+		}
+		return productsArray;
 	}// analyzeFile
 }
--- HW_4/Product.java
+++ HW_4/Product.java
@@ -1,12 +1,82 @@
-public class Product{
+import java.util.ArrayList;
+
+public class Product implements Comparable<Product> {
+	// member
 	private String name;
 	private double average;
-	private int maxRate;
-	private int minRate;
-	private double standardDeviation;
+	private ArrayList< Integer > rateList;
+	// constructor
 	Product () {
 		name = new String();
-		average = standardDeviation = 0.0;
-		maxRate = minRate = 0;
+		rateList = new ArrayList<Integer>();
+		average = -1.0;
+	}
+	// function
+	public void printFormatted () {
+		System.out.printf("[%-25s%.2f, %d, %d, %.2f]%n"
+				, getName() + ",", getAverage()
+				, getMaxRate(), getMinRate()
+				, getStandardDeviation() );
+	}
+	public int compareTo (Product x) {
+		if( x.getAverage() > this.getAverage() )
+			return 1;
+		else
+			return -1;
+	}
+	//// set
+	public void setName (String input) {
+		name = input;
+	}
+	public void addRate (int input) {
+		rateList.add( input );
+		this.average = (-1.0);
+	}
+	//// get
+	public String getName () {
+		return name;
+	}
+	public double getAverage () {
+		if ( this.average == (-1.0) ) {
+			double average = 0.0;
+			for ( int i=0; i<rateList.size(); ++i ) {
+				average += rateList.get(i);
+			}
+			average /= rateList.size();
+			this.average = average;
+			return average;
+		}
+		else {
+			return this.average;
+		}
+	}
+	public int getMaxRate () {
+		if ( rateList.size() == 0 )
+			return 0;
+		int maxRate = Integer.MIN_VALUE;
+		for ( int i=0; i<rateList.size(); ++i ) {
+			maxRate = Integer.max(maxRate, rateList.get(i));
+		}
+		return maxRate;
+	}
+	public int getMinRate () {
+		if ( rateList.size() == 0 )
+			return 0;
+		int minRate = Integer.MAX_VALUE;
+		for ( int i=0; i<rateList.size(); ++i ) {
+			minRate = Integer.min(minRate, rateList.get(i));
+		}
+		return minRate;
+	}
+	public double getStandardDeviation () {
+		double standardDeviation = 0.0;
+		double mean = getAverage();
+		for (int i=0; i<rateList.size(); ++i) {
+			double tmp = rateList.get(i) - mean;
+			standardDeviation += (tmp * tmp);
+		}
+		standardDeviation /= (rateList.size() - 1);
+		standardDeviation = Math.sqrt( standardDeviation );
+		return standardDeviation;
 	}
 }
