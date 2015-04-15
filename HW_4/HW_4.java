dd4cd674bab0243db0205cb2ade4de9e06bdf82f
snakeneedy <snakeneedy@gmail.com>
2015-04-15 18:45:14 +0800

    U: HW_4/HW_4.java 刪除不必要的 debug 程式碼和註解

--- HW_4/HW_4.java
+++ HW_4/HW_4.java
@@ -6,26 +6,8 @@ import java.io.FileNotFoundException;
 
 public class HW_4 {
 	public static void main (String args[]) {
-		//ArrayList<String> filenameList = new ArrayList<String>();
-		//filenameList.add( "hw4_file1.csv" );
-		//filenameList.add( "hw4_file2.csv" );
-		//System.out.println("[debug] " + filenameList );
 		try {
 			Product[] productsArray = null;
-			//for ( int ti=0; ti<filenameList.size(); ++ti ) {
-			//	StringBuilder id = new StringBuilder(
-			//			Integer.toString( ti + 1 )
-			//		);
-			//	while ( id.length() < 3 ) {
-			//		id.insert( 0, "0" );
-			//	}
-			//	System.out.println("---" + id.toString() + "---");
-			//	productsArray = analyzeFile( filenameList.get(ti) );
-			//	Arrays.sort( productsArray );
-			//	for (int i=0; i<productsArray.length; ++i) {
-			//		productsArray[i].printFormatted();
-			//	}
-			//}
 			for ( int ti=0; ti<args.length; ++ti ) {
 				StringBuilder id = new StringBuilder(
 						Integer.toString( ti + 1 )
@@ -51,21 +33,14 @@ public class HW_4 {
 		String inputLine = null;
 		String[] nameArray = null;
 		String[] rateStringArray = null;
-//System.out.println("[debug] content in '" + filename + "'");
-//while( inputScanner.hasNextLine() ) {
-//	System.out.println( inputScanner.nextLine() );
-//}
 		// name
 		if ( inputScanner.hasNextLine() ) {
 			inputLine = inputScanner.nextLine();
 			nameArray = inputLine.split(",");
 		}
-		//System.out.println("[debug] nameArray.length = " + nameArray.length);
 		Product[] productsArray = new Product[ nameArray.length ];
-		//System.out.println("[debug] productsArray.length = " + productsArray.length);
 		for ( int i=0; i<nameArray.length; ++i ) {
 			productsArray[i] = new Product();
-			//productsArray[i].printFormatted();//TODO debug
 			productsArray[i].setName( nameArray[i] );
 		}
 		// rate
