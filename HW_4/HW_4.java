abf42447345164ea2a93074d4582392b67256b21
snakeneedy <snakeneedy@gmail.com>
2015-04-15 18:29:40 +0800

    C: HW_4 基本 code

---
+++ HW_4/HW_4.java
@@ -0,0 +1,11 @@
+public class HW_4 {
+	public static void main (String args[]) {
+		;
+	}// main
+	static Product[] analyzeFile (String filename)
+			throws Exception {
+		Product[] productArray = null;
+		// TODO
+		return productArray;
+	}// analyzeFile
+}
---
+++ HW_4/Product.java
@@ -0,0 +1,12 @@
+public class Product{
+	private String name;
+	private double average;
+	private int maxRate;
+	private int minRate;
+	private double standardDeviation;
+	Product () {
+		name = new String();
+		average = standardDeviation = 0.0;
+		maxRate = minRate = 0;
+	}
+}
