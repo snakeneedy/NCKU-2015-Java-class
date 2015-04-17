f2f359f709d53104bc657c0c7456c937f4069ead
snakeneedy <snakeneedy@gmail.com>
2015-04-17 13:28:21 +0800

    U: HW_5/HW_5.java basic code

---
+++ HW_5/HW_5.java
@@ -0,0 +1,12 @@
+public class HW_5 {
+	// dictionary.txt hw5_input.txt
+	public static void main (String[] args) {
+	}
+
+	static void printResult (String input, String[] result) {
+		System.out.print(input + " => ");
+		for (int i=0; i<result.length; ++i) {
+			System.out.print( result[i] + ", ");
+		}
+	}
+}
